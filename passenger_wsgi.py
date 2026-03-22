788d955104373e0d2d9e4cab54e8196f269d9f9c
anonpls <anonpls10@gmail.com>
2026-03-22 19:08:10 +0300

    deleted .restart-app from index and changed directory in passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,7 +5,7 @@ INTERP = "/var/www/u3354621/data/flaskenv/bin/python"
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
-project_dir = '/var/www/u3354621/data/www/rivagedor.ru'
+project_dir = '/var/www/u3354621/data/www/wills-details.ru'
 sys.path.insert(0, project_dir)
 os.chdir(project_dir)
 
